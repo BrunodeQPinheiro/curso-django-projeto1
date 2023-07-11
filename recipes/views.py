4e325752ec713c807ca1e3e762c644a5ee4716c1
Bruno de Queiroz Pinheiro <brunoban2010@hotmail.com>
2023-07-11 13:24:03 -0300

    organizando para começar a programar

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # HTTP Request
 def home(request):
     return render(request, 'recipes/home.html')
     # return HTTP Response
 
-# HTTP Request
-def contato(request):
-    return HttpResponse('CONTATO')
-    # return HTTP Response
-
-    # HTTP Request
-def sobre(request):
-    return HttpResponse('SOBRE')
-    # return HTTP Response
