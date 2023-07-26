6b37b809d01a906e3df2fd0851678064518db3cb
Bruno de Queiroz Pinheiro <brunoban2010@hotmail.com>
2023-07-26 15:57:25 -0300

    configurando base de global

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html')
     # return HTTP Response
 
+
+# HTTP Request
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
+    # return HTTP Response
